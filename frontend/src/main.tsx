ea019a4a51e2ba90485f6eac8e2faec80e07fd20
sumitbhardwajcs23 <sumitbhardwaj2227@gmail.com>
2026-03-30 00:19:11 +0530

    Switch to HashRouter to fix Amplify SPA routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { ThemeProvider } from 'next-themes'
 import App from './App'
@@ -8,11 +8,11 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <HashRouter>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <App />
         <Toaster position="top-right" />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
